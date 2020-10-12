63c27cf35db868b09d1df4db688c3edbf46806b4
Brian Nkurunungi <nkurunungib@gmail.com>
2020-10-12 10:01:39 -0500

    Custom Error Page - Spring Boot

---
+++ src/main/java/com/example/springboot_505/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springboot_505;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "base";
+    }
+
+}
